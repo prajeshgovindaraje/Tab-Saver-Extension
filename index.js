70c46841131e9e281e88c579f9605dfabc866716
prajesh <prajeshgovindaraje@gmail.com>
2023-04-28 04:56:47 +0530

    create a working model

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ let myLeads = []
 let deleteButton = document.getElementById("delete-btn")
 let saveButton = document.getElementById("save-btn")
 let unOrdList = document.getElementById("un-list")
+let currrentTab = document.getElementById("current-tab-btn")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads",myLeads))
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+
+
+currrentTab.addEventListener("click",function(){
+
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        console.log(tabs[0].url);
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+        render(myLeads)
+
+
+    });
+
+})
 
 saveButton.addEventListener("click",function(){
     let myInput = document.getElementById("inp-field").value
@@ -39,7 +55,7 @@ function render(){
     console.log(myLeads)
 
     for(let i = 0 ; i < myLeads.length; i++){
-         listItems += `<a href="${myLeads[i]}><li> ${myLeads[i]} </li></a>`
+         listItems += `<a href="${myLeads[i]}" target="_blank"><li> ${myLeads[i]} </li></a>`
     }
 
     unOrdList.innerHTML = listItems
